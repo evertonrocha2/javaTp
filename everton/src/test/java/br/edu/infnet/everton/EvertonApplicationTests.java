e1dd57b497f163a04138c119833c39f56ba8a994
evertonrocha2 <evertondarocha2018@gmail.com>
2024-02-19 19:43:52 -0300

    Projeto Java - TP1 - Everton

--- everton/src/test/java/br/edu/infnet/everton/EvertonApplicationTests.java
+++ everton/src/test/java/br/edu/infnet/everton/EvertonApplicationTests.java
@@ -11,9 +11,15 @@ import br.edu.infnet.everton.model.domain.Estudante;
 class EvertonApplicationTests {
 	
 	@Test
-	void aprovadoOuNao() {
+	void aprovadoErro() {
 		Estudante everton = new Estudante("Everton", 21, "Java", 10.0);
 		assertEquals("Reprovado", everton.getStatus());
+	}
+	
+	@Test
+	void reprovadoCerto() {
+		Estudante leticia = new Estudante("Leticia", 21, "C#", 4.0);
+		assertEquals("Reprovado", leticia.getStatus());
 	}
 
 }
